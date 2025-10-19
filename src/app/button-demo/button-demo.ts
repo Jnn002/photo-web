718751f55a3fe20d0affa0fef47886ea2ad050f8
Jnn002 <jyaxp1@miumg.edu.gt>
2025-10-18 23:24:43 -0600

    set primeng

---
+++ src/app/button-demo/button-demo.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+    selector: 'app-button-demo',
+    imports: [ButtonModule],
+    templateUrl: './button-demo.html',
+    styleUrl: './button-demo.css',
+})
+export class ButtonDemo {}
